26f2de643b52243e6b80b8f3ad655119ef00aca6
Vitalii <flatronsamatron@gmail.com>
2024-05-20 22:06:51 +0300

    add request class

---
+++ framework/Request.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+namespace Framework;
+
+readonly class Request
+{
+    public function __construct(
+            private array $getParams,
+            private array $postData,
+            private array $cookies,
+            private array $files,
+            private array $server,
+    ) {
+    }
+
+    public static function createFromGlobals(): static
+    {
+        return new static($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+    }
+
+    public function getParam($key): string
+    {
+        return $this->getParams[$key];
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
+use \Framework\Request;
+
+$request = Request::createFromGlobals();
+
+dd($request);
+
 //router
 
 //logic
